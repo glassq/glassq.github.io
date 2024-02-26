d079efc065cad51f496b1e4d64cdc2500c6c162e
Tony <santiago.montesb@gmail.com>
2024-02-26 00:45:35 -0500

    migration to React

---
+++ README.md
@@ -0,0 +1,5 @@
+# GlassQb
+
+Check out our web page [here](https://glassqb.github.io)!
+
+Contact us for more information at [this email](l.rojasb@uniandes.edu.co).
---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div class="main-content">
+          <h1>GlassQb</h1>
+          <p>coming soon...</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
